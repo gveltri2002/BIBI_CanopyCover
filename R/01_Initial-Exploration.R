cb3d6c4299e81a83d52baec73b8acea0415b5ffb
gveltri2002 <gabeveltri@gmail.com>
2026-04-12 17:55:11 -0700

    Setting up files/structure

---
+++ R/01_Initial-Exploration.R
@@ -0,0 +1,12 @@
+# === === === === === === === === === === === === === === ===  
+# Created by Gabe Veltri 4/12/2026
+# Project: CanopyCover
+# Goal: Initial exploration of canopy cover data for 2025 sampling year
+# === === === === === === === === === === === === === === === 
+
+#### PREP WORK ####
+
+# Load Packages
+
+# Import Data
+
